be8aeff4ae397b0560926b2d5698c9f76a898e28
AaronFerraz <aaronferrazdpn@gmail.com>
2025-03-22 03:30:34 -0300

    Configuração inicial das classes

--- src/Eachare.java
+++ src/Eachare.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Eachare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+
+        do{
+            System.out.println("Escolha um comando: ");
+            System.out.println("\t\t[1] Listar peers"
+                    + "\n\t\t[2] Obter peers"
+                    + "\n\t\t[3] Listar arquivos locais"
+                    + "\n\t\t[4] Buscar arquivos"
+                    + "\n\t\t[5] Exibir estatisticas"
+                    + "\n\t\t[6] Alterar tamanho de chunk"
+                    + "\n\t\t[9] Sair");
+            System.out.print("> ");
+            opcao = sc.nextInt();
+
+            switch (opcao){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 9:
+                    opcao = 0;
+                    break;
+                default:
+                    System.out.println("Não existe essa opção");
+            }
+        }while(opcao != 0);
+    }
 }
