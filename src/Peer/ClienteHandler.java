fdc8afcc4465f0f41fe2f9efb3d027a2e2732ba8
Renan Mastropaolo <re.gon.mas@usp.br>
2025-03-25 21:01:22 -0300

    Recebimento de mensagem

--- src/Peer/ClienteHandler.java
+++ src/Peer/ClienteHandler.java
@@ -1,31 +1,10 @@
 package Peer;
 
-import Protocol.MessageHandler;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
-public class ClienteHandler implements Runnable{
-    private Socket socket;
-    private Peer peer;
-    private MessageHandler messageHandler;
+public class ClienteHandler {
+    public static void helloMessage(Socket clientSocket, String message) {
 
-    public ClienteHandler(Socket socket, Peer peer){
-        this.socket = socket;
-        this.peer = peer;
     }
 
-    @Override
-    public void run() {
-        try(BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))){
-            String message;
-            while((message = in.readLine()) != null){
-                messageHandler.receiveMessage(mensagem);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 }
--- src/Peer/ServerThread.java
+++ src/Peer/ServerThread.java
@@ -4,21 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ServerThread implements Runnable{
-    private PeerInfo peer;
+public class ServerThread {
 
-    public ServerThread(PeerInfo peer){
-        this.peer = peer;
-    }
-
-    @Override
-    public void run(){
-        try(ServerSocket socketServer = new ServerSocket(peer.getPort());
-            Socket clientSocket = socketServer.accept()){
-
-            new Thread(new ClientThread(clientSocket, peer)).start();
-        }catch (IOException e){
-
-        }
-    }
 }
